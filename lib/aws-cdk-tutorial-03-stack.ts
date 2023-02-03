89e75c73ea385bdfab48fedea6dee3129e2de1cf
Ash Belmokadem <Ash.Belmokadem@VodafoneZiggo.com>
2023-02-03 06:41:37 +0100

    Copy to GitHub

--- lib/aws-cdk-tutorial-03-stack.ts
+++ lib/aws-cdk-tutorial-03-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkTutorial03Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new Table(this, 'Table', {
+      partitionKey: {
+        type: AttributeType.STRING,
+        name: 'id'
+      },
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkTutorial03Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const notificationHandler = new NodejsFunction(this, 'Function', {
+      environment: {
+        TABLE_NAME: table.tableName
+      }
+    })
+
+    table.grantReadData(notificationHandler)
   }
 }
